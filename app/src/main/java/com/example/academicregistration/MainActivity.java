33bc3f09dab303927898f823ac5d3e7af67e386e
Bobo FB <https://github.com/Bobo-cryto/Academic-Registration>
2021-06-08 02:07:50 +0100

    Academic Registration

--- app/src/main/java/com/example/academicregistration/MainActivity.java
+++ app/src/main/java/com/example/academicregistration/MainActivity.java
@@ -7,31 +7,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     public Button btnSignin;
-    public TextView etFname, etLname, etPassword, etEmail, etAreaCode, etNumber;
+    public EditText etFname, etPassword, etEmail, etdob, etNumber;
+    public  TextView gologin;
+    public EditText User_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etFname = (TextView)findViewById(R.id.etFname);
-        etLname = (TextView)findViewById(R.id.etLname);
-        etPassword = (TextView)findViewById(R.id.etPassword);
-        etEmail = (TextView)findViewById(R.id.etEmail);
-        etAreaCode = (TextView)findViewById(R.id.edtAreaCode);
-        etNumber = (TextView)findViewById(R.id.etNumber);
-        btnSignin = (Button) findViewById(R.id.btnSingin);
+        etFname = (EditText)findViewById(R.id.etFname);
+        User_name = (EditText) findViewById(R.id.rUSerName);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etdob = (EditText)findViewById(R.id.edtDOB);
+        etNumber = (EditText)findViewById(R.id.etNumber);
 
-        btnSignin.setOnClickListener(new View.OnClickListener() {
+        btnSignin = (Button) findViewById(R.id.btnSingin);
+        gologin = (TextView)findViewById(R.id.goLogin);
+
+
+        gologin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
-                Intent inter = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(inter);
+                Toast.makeText(MainActivity.this, "Input your Login Credentials", Toast.LENGTH_LONG).show();
+                Intent intergoR = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intergoR);
             }
         });
+
+    }
+
+    public void onSignin(View view) {
+        String FName = etFname.getText().toString();
+        String UserName = User_name.getText().toString();
+        String password = etPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String number = etNumber.getText().toString();
+        String Dob = etdob.getText().toString();
+
+        String type = "signin";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, FName, UserName, password, email, number, Dob);
+        Toast.makeText(MainActivity.this, "Congrats, Registration into HTTTC Bambibi Successful!!!", Toast.LENGTH_SHORT).show();
+        Intent inter = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(inter);
     }
 }
--- app/src/main/java/com/example/academicregistration/SecondActivity.java
+++ app/src/main/java/com/example/academicregistration/SecondActivity.java
@@ -2,20 +2,52 @@ package com.example.academicregistration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+
 
 public class SecondActivity extends AppCompatActivity {
-    public EditText etEmailLg, etPasswordLg;
+    public EditText etUSerName, etPasswordLg;
     public Button btnLg;
+    public TextView goRegister, goAdmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         btnLg = (Button)findViewById(R.id.btnLg);
-        etEmailLg = (EditText)findViewById(R.id.etEmailLg);
+        etUSerName = (EditText)findViewById(R.id.etUSerName);
         etPasswordLg = (EditText) findViewById(R.id.etPasswordLg);
+        goRegister = (TextView)findViewById(R.id.goRegister);
+        goAdmin = (TextView)findViewById(R.id.goAdmin);
+
+        goRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent interMain = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(interMain);
+            }
+        });
+
+        goAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent interAd = new Intent(SecondActivity.this, LoginActivity.class);
+//                startActivity(interAd);
+            }
+        });
+    }
+
+    public void OnLogin(View view) {
+        String UserName = etUSerName.getText().toString();
+        String password = etPasswordLg.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, UserName, password);
     }
 }
